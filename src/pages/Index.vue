1b1829fab8ad85d9042551cadbbf06b078124c22
sora1129 <64789798+sora1129@users.noreply.github.com>
2022-05-17 18:18:13 +0900

    imort TodoForm

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,12 +1,18 @@
 <template>
 	<div>
 	<h1>Todoリスト</h1>
+  <todo-form />
 	</div>
 </template>
 
 <script>
+import TodoForm from '@/components/TodoForm';
+
 export default {
-name: 'Todo',
+  name: 'Todo',
+  components: {
+    TodoForm,
+  },
 };
 </script>
 
