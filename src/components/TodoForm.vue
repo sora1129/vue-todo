6e7d4b2ecdab3f4712dc418ef1ca74920d0c8ff9
sora1129 <64789798+sora1129@users.noreply.github.com>
2022-05-17 18:17:49 +0900

    create TodoForm.vue

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+    <b-container>
+      <b-input-group>
+        <b-form-input type="text" v-model="value"/>
+        <b-input-group-append>
+          <b-button variant="success">送信</b-button>
+        </b-input-group-append>
+      </b-input-group>
+    </b-container>
+	</div>
+</template>
+
+<script>
+export default {
+  name: 'TodoForm',
+  data() {
+    return {
+      value: '',
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
