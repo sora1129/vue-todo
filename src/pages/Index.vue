c1f722c64737230522e7bca214e0926e8b9c178b
sora1129 <64789798+sora1129@users.noreply.github.com>
2022-05-18 19:16:05 +0900

    import TodoList

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,16 +2,22 @@
 	<div>
 	<h1>Todoリスト</h1>
   <todo-form @handleParentAddTodo="handleParentAddTodo"/>
+  <todo-list
+  :todos="todos"
+  @handleParentDeleteTodo="handleParentDeleteTodo"
+  @handleParentCompleteTodo="handleParentCompleteTodo" />
 	</div>
 </template>
 
 <script>
 import TodoForm from '@/components/TodoForm';
+import TodoList from '@/components/TodoList';
 
 export default {
   name: 'Todo',
   components: {
     TodoForm,
+    TodoList,
   },
   data() {
     return {
@@ -21,8 +27,14 @@ export default {
   methods: {
     handleParentAddTodo(value) {
       if (value) {
-        this.todos.push({ text: value });
+        this.todos.unshift({ text: value, complete: false });
       }
+    },
+    handleParentDeleteTodo(index) {
+      this.todos.splice(index, 1);
+    },
+    handleParentCompleteTodo(index) {
+      this.todos[index].complete = !this.todos[index].complete;
     },
   },
 };
